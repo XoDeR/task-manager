67c67ae5711a1fe9e97dcd62c64a5f46be22524b
XoDeR <xoder.vs@gmail.com>
2025-12-12 09:47:44 +0100

    backend: basic auth using sanctum tokens: added routes for login, register, logout and corresponding controller functions

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,7 +1,14 @@
 <?php
 
+use App\Http\Controllers\Api\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+
+Route::prefix('v1')->group(function () {
+    Route::post('register', [AuthController::class, 'register']);
+    Route::post('login', [AuthController::class, 'login']);
+    Route::post('logout', [AuthController::class, 'logout']);
+});
 
 Route::get('/user', function (Request $request) {
     return $request->user();
