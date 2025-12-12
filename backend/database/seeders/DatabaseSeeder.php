8a135dbc104bd81a6f67ca50a5fcab90542eafd8
XoDeR <xoder.vs@gmail.com>
2025-12-12 13:53:33 +0100

    created test user and some tasks

--- backend/database/seeders/DatabaseSeeder.php
+++ backend/database/seeders/DatabaseSeeder.php
@@ -2,9 +2,11 @@
 
 namespace Database\Seeders;
 
+use App\Models\Task;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,11 +17,40 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        // Create test user with email john@example.com and password: password
+        $user = User::factory()->create([
+            'name' => 'John',
+            'email' => 'john@example.com',
+            'password' => Hash::make('password')
+        ]);
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        // And some sample tasks that belong to this user
+        Task::create([
+            'title' => 'Do groceries',
+            'description' => 'Go to the closest supermarket and buy bread, butter and vegetables',
+            'status' => 0,
+            'user_id' => $user->id,
+        ]);
+
+        Task::create([
+            'title' => 'Finish report',
+            'description' => 'Complete the report',
+            'status' => 1,
+            'user_id' => $user->id,
+        ]);
+
+        Task::create([
+            'title' => 'Workout',
+            'description' => 'Go to the gym',
+            'status' => 2,
+            'user_id' => $user->id,
+        ]);
+
+        Task::create([
+            'title' => 'Another task',
+            'description' => 'Very long description Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi. Nam eget dui. Etiam rhoncus. Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque sed ipsum. Nam quam nunc, blandit vel, luctus pulvinar, hendrerit id, lorem. Maecenas nec odio et ante tincidunt tempus. Donec vitae sapien ut libero venenatis faucibus. Nullam quis ante. Etiam sit amet orci eget eros faucibus tincidunt. Duis leo. Sed fringilla mauris sit amet nibh. Donec sodales sagittis magna. Sed consequat, leo eget bibendum sodales, augue velit cursus nunc,',
+            'status' => 0,
+            'user_id' => $user->id,
         ]);
     }
 }
