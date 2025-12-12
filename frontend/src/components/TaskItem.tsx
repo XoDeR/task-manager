3c36110a55a2cf313c90c64f9b5be08be90c2fd1
XoDeR <xoder.vs@gmail.com>
2025-12-12 12:32:24 +0100

    task item: accordion ui component used to display task description

--- frontend/src/components/TaskItem.tsx
+++ frontend/src/components/TaskItem.tsx
@@ -1,4 +1,6 @@
 import type { Task } from "@/types";
+import { Card, CardContent } from "./ui/card";
+import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from "./ui/accordion";
 
 interface Props {
   task: Task;
@@ -6,7 +8,23 @@ interface Props {
 
 const TaskItem = ({ task }: Props) => {
   return (
-    <div>{task.title}</div>
+    <Card className="h-full overflow-hidden py-1">
+      <CardContent>
+        <Accordion
+            type="single"
+            collapsible
+            className="w-3/4 mr-4"
+            defaultValue="item-1"
+          >
+            <AccordionItem value="item-1">
+              <AccordionTrigger><p className="text-lg text-foreground">{task.title}</p></AccordionTrigger>
+              <AccordionContent className="flex flex-col gap-4 text-balance">
+                <p className="text-sm text-foreground">{task.description}</p>
+              </AccordionContent>
+            </AccordionItem>
+          </Accordion>
+      </CardContent>
+    </Card>  
   )
 }
 
