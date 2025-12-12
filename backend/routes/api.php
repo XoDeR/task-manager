acc7c52e36befe5b899d6cf00e0efbebd5ac8e15
XoDeR <xoder.vs@gmail.com>
2025-12-12 09:59:37 +0100

    backend: tasks crud: generated API controller for tasks; routes for tasks protected with sanctum middleware

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\TaskController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -8,6 +9,12 @@ Route::prefix('v1')->group(function () {
     Route::post('register', [AuthController::class, 'register']);
     Route::post('login', [AuthController::class, 'login']);
     Route::post('logout', [AuthController::class, 'logout']);
+});
+
+Route::prefix('v1')->middleware(['auth:sanctum'])->group(function () {
+    Route::apiResource('tasks', TaskController::class, [
+        'only' => ['index', 'store', 'update', 'destroy']
+    ]);
 });
 
 Route::get('/user', function (Request $request) {
