8ded1736a9a443d1a9e1ca92fbf89eb68318d300
XoDeR <xoder.vs@gmail.com>
2025-12-12 12:41:31 +0100

    task item: display task status; option to change task status

--- frontend/src/components/TaskItem.tsx
+++ frontend/src/components/TaskItem.tsx
@@ -1,28 +1,82 @@
 import type { Task } from "@/types";
 import { Card, CardContent } from "./ui/card";
 import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from "./ui/accordion";
+import { useUpdateTask } from "@/hooks/useTasks";
+import { Button } from "./ui/button";
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "./ui/dropdown-menu";
+import { PencilIcon } from "lucide-react";
 
 interface Props {
   task: Task;
 }
 
 const TaskItem = ({ task }: Props) => {
+  
+  const updateMutation = useUpdateTask();
+
+  const changeStatus = (status: number) => {
+    updateMutation.mutate(
+      { ...task, status: status },
+    );
+  }
+
+  const getOtherStatuses = (status: number): number[] => {
+    const allStatuses = [0, 1, 2];
+    return allStatuses.filter((s) => s !== status);
+  }
+
+  const getStatusName = (status: number): string => {
+    let result = "Unknown Status"
+    switch (status) {
+      case 0:
+        result = "To Do"
+        break;
+      case 1:
+        result = "In Progress"
+        break;
+      case 2:
+        result = "Done"
+        break;
+      default:
+        break;
+    }
+    return result;
+  }
+  
   return (
     <Card className="h-full overflow-hidden py-1">
       <CardContent>
-        <Accordion
-            type="single"
-            collapsible
-            className="w-3/4 mr-4"
-            defaultValue="item-1"
-          >
-            <AccordionItem value="item-1">
-              <AccordionTrigger><p className="text-lg text-foreground">{task.title}</p></AccordionTrigger>
-              <AccordionContent className="flex flex-col gap-4 text-balance">
-                <p className="text-sm text-foreground">{task.description}</p>
-              </AccordionContent>
-            </AccordionItem>
-          </Accordion>
+        <div className="flex">
+          <Accordion
+              type="single"
+              collapsible
+              className="w-3/4 mr-4"
+              defaultValue="item-1"
+            >
+              <AccordionItem value="item-1">
+                <AccordionTrigger><p className="text-lg text-foreground">{task.title}</p></AccordionTrigger>
+                <AccordionContent className="flex flex-col gap-4 text-balance">
+                  <p className="text-sm text-foreground">{task.description}</p>
+                </AccordionContent>
+              </AccordionItem>
+            </Accordion>
+            <div className="flex justify-end w-1/4">
+              <Button className="mx-1 mt-3" variant="outline">{getStatusName(task.status)}</Button>
+              <DropdownMenu>
+                <DropdownMenuTrigger asChild className="mt-3">
+                  <Button variant="outline"><PencilIcon/></Button>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent className="w-4 flex flex-col justify-center" align="center">
+                  {getOtherStatuses(task.status).map((st) => (
+                    <DropdownMenuItem key={st} asChild>
+                      <Button onClick={() => { changeStatus(st) }} variant="outline">
+                        {getStatusName(st)}
+                      </Button>
+                    </DropdownMenuItem>))}
+                </DropdownMenuContent>
+              </DropdownMenu>
+            </div>
+          </div>
       </CardContent>
     </Card>  
   )
