34e275595bf1efed06eb4f4e3a7ce9f6cc683c52
XoDeR <xoder.vs@gmail.com>
2025-12-12 11:58:28 +0100

    added modal component based on shadcn dialog; will be used to display task create form

---
+++ frontend/src/components/Modal.tsx
@@ -0,0 +1,43 @@
+import * as React from "react";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+} from "@/components/ui/dialog";
+
+export interface ModalProps {
+  open: boolean;
+  onOpenChange: (open: boolean) => void;
+  title?: string;
+  description?: string;
+  children: React.ReactNode;
+  className?: string;
+}
+
+export function Modal({
+  open,
+  onOpenChange,
+  title,
+  description,
+  children,
+  className,
+}: ModalProps) {
+  return (
+    <Dialog open={open} onOpenChange={onOpenChange}>
+      <DialogContent className={className}>
+        {(title || description) && (
+          <DialogHeader>
+            {title && <DialogTitle>{title}</DialogTitle>}
+            {description && (
+              <DialogDescription>{description}</DialogDescription>
+            )}
+          </DialogHeader>
+        )}
+
+        <div className="py-2">{children}</div>
+      </DialogContent>
+    </Dialog>
+  );
+}
