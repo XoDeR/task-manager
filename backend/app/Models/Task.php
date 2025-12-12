ea8ab39cda3b97c226ff9321031a46189af7cb2f
XoDeR <xoder.vs@gmail.com>
2025-12-12 09:55:38 +0100

    backend: tasks crud: user-task one-to-many relationship

--- backend/app/Models/Task.php
+++ backend/app/Models/Task.php
@@ -21,4 +21,9 @@ class Task extends Model
         'description',
         'status',
     ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
--- backend/database/migrations/2025_12_12_084838_create_tasks_table.php
+++ backend/database/migrations/2025_12_12_084838_create_tasks_table.php
@@ -17,6 +17,8 @@ return new class extends Migration
             $table->text('description');
             $table->tinyInteger('status')->default(0);
             $table->timestamps();
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
         });
     }
 
